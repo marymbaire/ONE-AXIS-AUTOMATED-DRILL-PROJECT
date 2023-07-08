b0fbab78c79068cdd3c62140ff4aeb0959004216
marymbaire <marymbugua5821@gmail.com>
2023-07-08 10:39:06 +0300

    all sensors in one simulation

--- CODES/one_axis_drill_codes_arduino/Proteus simulation codes/run_accelerometer_proteus/run_accelerometer_proteus.ino
+++ CODES/one_axis_drill_codes_arduino/Proteus simulation codes/run_accelerometer_proteus/run_accelerometer_proteus.ino
@@ -1,30 +1,24 @@
-const int potXPin = A0; // Potentiometer for X-axis
-const int potYPin = A1; // Potentiometer for Y-axis
-const int potZPin = A2; // Potentiometer for Z-axis
+const int potPinx = A0; // Connect the potentiometer to analog pin A0
+const int potPiny = A1; // Connect the potentiometer to analog pin A0
+const int potPinz = A2; // Connect the potentiometer to analog pin A0
 void setup() {
-  // put your setup code here, to run once:
-Serial.begin (9600);
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-// Read the potentiometer values
-  int potXValue = analogRead(potXPin);
-  int potYValue = analogRead(potYPin);
-  int potZValue = analogRead(potZPin);
-
-  // Map the potentiometer values to a range of -100 to +100
-  float xDisplacement = map(potXValue, 0, 1023, -4, 4);
-  float yDisplacement = map(potYValue, 0, 1023, -4, 4);
-  float zDisplacement = map(potZValue, 0, 1023, -4, 4);
-
-  // Print the displacements on the serial monitor
-  Serial.print("X Displacement: ");
+  int potValuex = analogRead(potPinx);
+  int potValuey = analogRead(potPiny);
+  int potValuez = analogRead(potPinz);
+  
+  float xDisplacement = map(potValuex, 0, 1023, -4, 4);
+  float yDisplacement = map(potValuey, 0, 1023, -4, 4);
+  float zDisplacement = map(potValuez, 0, 1023, -4, 4);
+  Serial.print("Displacement X axis: ");
   Serial.print(xDisplacement);
-  Serial.print("\tY Displacement: ");
+   Serial.print("Displacement Y axis: ");
   Serial.print(yDisplacement);
-  Serial.print("\tZ Displacement: ");
+   Serial.print("Displacement Z axis : ");
   Serial.println(zDisplacement);
-
-  delay(500); // Delay for smoother serial monitor output (adjust if needed)
+  
+  delay(500); // Adjust the delay as per your requirement
 }
